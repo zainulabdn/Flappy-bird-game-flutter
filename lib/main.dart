871c48c8048885595921d4e8e317d9647f8d6a40
Zain Ul Abideen <80303452+zainulabdn@users.noreply.github.com>
2024-01-05 22:40:48 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:flappy_bird_game/screens/main_menu_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'screens/game_over_screen.dart';
-
+///for gaming
 Future<void> main() async {
   SystemChrome.setSystemUIOverlayStyle(
       const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
